7509afb2a7c0893cfed09721ae8fc3f4d6f1db94
AntonEstrela <anton.estrela@gmail.com>
2019-03-18 19:13:25 +0100

    los datos ya se meten en el recyclerview +control de errores +swipe down to reftesh

--- app/src/main/java/com/example/restclient/MainActivity.java
+++ app/src/main/java/com/example/restclient/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.restclient;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -17,26 +21,52 @@ import com.example.restclient.TrackService;
 import static com.example.restclient.TrackService.retrofit;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        swipeRefreshLayout = findViewById(R.id.swiperefresh);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        swipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        update();
+                    }
+                }
+        );
     }
     public void onClick(View view){
-        TrackService trackService = retrofit.create(TrackService.class);
+        update();
+    }
+    public void update(){
+        final TrackService trackService = retrofit.create(TrackService.class);
         Call<List<Track>> call = trackService.tracks();
         call.enqueue(new Callback<List<Track>>() {
             @Override
             public void onResponse(Call<List<Track>> call, Response<List<Track>> response) {
-                for (Track t: response.body()) {
-
-                }
+                mAdapter = new MyAdapter(response.body());
+                recyclerView.setAdapter(mAdapter);
+                swipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
             public void onFailure(Call<List<Track>> call, Throwable t) {
-                // handle failure
+                Toast.makeText(MainActivity.this,"Cannot get track list: " + t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
